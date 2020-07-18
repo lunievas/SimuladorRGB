8de7f9d3ec247d408d5ddaef077d3ffd5d66511f
lunievas <luisa.nievas94@gmail.com>
2020-07-18 18:50:40 -0300

    criando a função que pega o valor do input range vermelho e coloca no input texto

--- js/script.js
+++ js/script.js
@@ -1,11 +1,25 @@
 // window.addEventListener('load', start)
 
+var redRange = document.querySelector('#red');
+var greenRange = document.querySelector('#green');
+var blueRange = document.querySelector('#blue');
+var textRed = document.querySelector('#valorRed');
+var textGreen = document.querySelector('#valorGreen');
+var textBlue = document.querySelector('#valorBlue');
+var blocoDeCor = document.querySelector('#cor');
 
 
 
 function start(){
-    console.log('carregou')
+    redRange.addEventListener('input', mudandoValorInputRed)
+}
+
+function mudandoValorInputRed(event){
+   var valorAtual = event.target.value;
+    textRed.value = valorAtual;
 }
 
 start();
+
+
 
